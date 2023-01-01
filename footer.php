9ccb5389bf6fd38431830b5155d46341b42b5385
tuyetnguyen03 <23A4040153@hvnh.edu.vn>
2023-01-02 00:10:26 +0700

    add prd to cart

---
+++ footer.php
@@ -0,0 +1,66 @@
+
+
+
+
+	<!-- footer -->
+	<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi</h2>
+				<p class="footer-main mb-4">
+					© 2022. Công ty TNHH Smart Electronics. GPDKKD: 0302309845 do sở KH & ĐT TP.HCM cấp ngày 21/05/2001<br>Địa chỉ: Số 12 Chùa Bộc, Quận Đống Đa, Hà Nội<br>Điện thoại: 028.39505060 - Email: info@smartelectronics.com.vn<br>Chịu trách nhiệm nội dung: Nhóm 5</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí vận chuyển</h3>
+								<p>Đơn hàng trên 100$</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Vận chuyển nhanh</h3>
+								<p>Toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Tin cậy</h3>
+								<p>Sự đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		
+		<!-- //footer third section -->
+		
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 2022 Công ty TNHH Smart Electronics. All rights reserved | Design by
+				<a href="http://w3layouts.com"> W3layouts.</a>
+			</p>
+		</div>
+	</div>
+	<!-- //copyright -->
---
+++ ketnoi.php
@@ -0,0 +1,12 @@
+<?php
+	$con = new mysqli("localhost","root","","shop");
+	if($con->connect_error){
+		die("Lỗi kết nối".$con->connect_error);
+
+	}
+	else
+	{
+		echo "Kết nối thành công";
+	}
+
+?>
